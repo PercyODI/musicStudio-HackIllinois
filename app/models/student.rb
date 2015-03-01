77cc37df79b16532493fe207f64e8fb5d5b0e275
percyodi <pearse.2008@gmail.com>
2015-03-01 11:03:10 +0000

    Minor cosmetic fixes. Added titleize to Student and Teacher elements before saving to database

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ActiveRecord::Base
     belongs_to :teacher
     has_many :logs
+    
+    before_save :upperCase
+    
+    def upperCase
+        self.name = self.name.titleize
+    end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,11 @@
 class Teacher < ActiveRecord::Base
     has_many :students
+    
+    before_save :upperCase
+    
+    def upperCase
+        self.name = self.name.titleize
+        self.primaryInstrument = self.primaryInstrument.titleize
+        self.location = self.location.titleize
+    end
 end
